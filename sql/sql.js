155cfa62bd08d982d9ec46ab7a9c19f515349499
Felipe - Notebook <sharpion.k@gmail.com>
2020-05-05 21:31:31 +0100

    More readable code.

--- sql/sql.js
+++ sql/sql.js
@@ -1,11 +1,21 @@
 var mysql = require('mysql');
+var port = process.env.PORT || 4205;
 
-var connection = mysql.createConnection({
-  host: process.env.SQL_HOST,
-  user: process.env.SQL_USER,
-  password: process.env.SQL_PASS,
-  database: process.env.SQL_DB
-});
+if (port === 4205) {
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'omegafox_shopping'
+  });
+} else {
+  var connection = mysql.createConnection({
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DB
+  });
+}
 
 connection.connect(function (err) {
   if (err) throw err;
