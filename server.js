e6b9d77853608971ea8aeec5782cb610f8ecf925
Felipe (Home) <sharpion.k@gmail.com>
2022-04-15 17:14:09 +0100

    Updating configs to work on A2 Hosting

--- server.js
+++ server.js
@@ -1,16 +1,74 @@
 const express = require('express'),
     app = express(),
     cors = require('cors'),
-    port = process.env.PORT || 8081;
+    session = require('express-session'),
+    MySQLStore = require('express-mysql-session')(session),
+    SQLConfig = require('./app/constants/sqlConfig'),
+    dotenv = require('dotenv'),
+    http = require('http'),
+    https = require('https'),
+    fs = require('fs');
 
+dotenv.config();
+
+const allowedOrigins = [
+    'https://localhost',
+    'http://localhost',
+    'https://localhost:3000',
+    'http://localhost:3000',
+    /\.omegafox\.me$/
+];
+
+app.use(cors({
+    credentials: true,
+    origin: allowedOrigins
+}));
 app.options('*', cors());
-app.use(cors());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(port);
+const sessionSecret = process.env.SESSION_SECRET;
 
-console.log('API server started on: ' + port);
+const sevenDays = 7 * 24 * 60 * 60 * 1000;
+const sessionSettings = {
+    secret: sessionSecret,
+    cookie: {
+        maxAge: sevenDays
+    },
+    resave: true,
+    saveUninitialized: false,
+    rolling: true,
+};
 
-var routes = require('./app/routes/appRoutes'); //importing route
-routes(app); //register the route
+let server;
+let serverPort;
+const environment = app.get('env');
+
+sessionSettings.cookie.secure = true;
+sessionSettings.cookie.sameSite = 'none';
+app.set('trust proxy', 1) // trust first proxy
+
+if (environment === 'production') {
+    serverPort = 0;
+    server = http.createServer(app);
+} else {
+    serverPort = 63768;
+    server = https.createServer({
+        key: fs.readFileSync('certs/key.pem'),
+        cert: fs.readFileSync('certs/cert.pem')
+    }, app);
+}
+
+sessionSettings.store = new MySQLStore(SQLConfig.returnConfig(environment));
+
+app.use(session(sessionSettings));
+
+server.listen(serverPort, function () {
+    console.log(`CORS-enabled web server listening on port ${server.address().port} at ${environment} environment`);
+});
+
+var routes = require('./app/routes/appRoutes'); // importing route 
+routes(app); // register the routes
+
+module.exports = app;
